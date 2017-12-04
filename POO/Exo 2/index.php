79b86233c4979aed866976061530553ca1b77f31
Unknown <n.vanroost@gmail.com>
2017-12-04 10:44:23 +0100

    i give up

--- POO/Exo 2/index.php
+++ POO/Exo 2/index.php
@@ -1,16 +1,16 @@
 <?php
 class Voiture{
-  function voiture(){
-    $immat;
-    $date;
-    $km;
-    $model;
-    $brand;
-    $color;
-    $weight;
-  }
+    private $immat;
+    private $date;
+    public $km;
+    public $newkm;
+    public $model;
+    private $brand;
+    public $color;
+    public $weight;
+    public $reserved;
   function reserved(){
-    $reserved = true;
+    $this->reserved = true;
     if ($reserved == true){
       echo "reserved";
     } else if ($reserved == false){
@@ -18,7 +18,7 @@ class Voiture{
     }
   }
   function utility(){
-    global $weight;
+    $this->weight=3000;
     if ($weight <= 3499) {
       echo "Vehicule utilitaire";
     } else if ($weight >= 3500){
@@ -28,7 +28,7 @@ class Voiture{
     }
   }
   function pays(){
-    global $immat;
+    $this->immat="BE 045 BGS";
     $im = substr($immat, 2);
     if ($im == "BE"){
       echo "Belgique";
@@ -40,8 +40,49 @@ class Voiture{
       echo "inconnu";
     }
   }
+  function usage(){
+    $this->km=80000;
+    if ($this->km<=100000){
+      echo "low";
+    } else if ($this->km>100000 and $this->km<=200000){
+      echo "middle";
+    } else if ($this->km>200000){
+      echo "high";
+    }
+  }
+  function age(){
+    $this->date=2000;
+    echo $this-> date(Y) - $date;
+  }
+  function drive(){
+    $this->km=160000;
+    $newkm = $this->km + 100000;
+    echo $newkm," km";
+    global $newkm;
+  }
+  function newusage(){
+    global $newkm;
+    if ($newkm<=100000){
+      echo "low";
+    } else if ($newkm>100000 and $newkm<=200000){
+      echo "middle";
+    } else if ($newkm>200000){
+      echo "high";
+    }
+  }
 }
-$audi= new Voiture();
-$audi-> $model = "audi";
-$
+$audi = new Voiture();
+$audi->model = "audi";
+echo $audi->model;
+echo '</br>';
+echo $audi->km = 80000," km";
+echo '</br>';
+echo $audi->usage();
+echo '</br>';
+echo $audi->drive();
+echo '</br>';
+echo $audi->newusage();
+
+//ok i give up...
+//stupid language...
  ?>
